39a4487e23b2b33988906648bfef6c8d1fcb45f5
Sidney Cardozo <scooby123sid@gmail.com>
2017-07-12 00:39:49 +0000

    got the css in for the twitter feed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,3 +4,25 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+$(document).ready(function() {
+
+  function createTweetElement (tweetObject) {
+    <article class="tweet">
+          <header class="clear-fix">
+            <img src="http://lorempixel.com/60/60/" />
+            <h2>Bill Fields</h2>
+            <span> @MrFields </span>
+          </header>
+          <main>
+             Little tweet here
+          </main>
+          <footer>
+            <p>
+              <span> 10 days ago </span>
+              <span class="icons"> &#10084; &#32; &#9993; &#32;  &#9873; </span>
+            </p>
+          </footer>
+        </article>
+ }
+
+}
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,7 +2,13 @@ $( document ).ready(function() {
   const max = 140;
   $(".new-tweet textarea").on("keyup", function(){
     let stringLength = $(this).val().length;
-    $(this).parents().children(".counter").text(max - stringLength);
+    const counter = $(this).parents().children(".counter");
+    counter.text(max - stringLength);
+    if(max - stringLength < 0){
+      counter.addClass("redText");
+    }else{
+      counter.removeClass("redText");
+    }
   })
 });
 
